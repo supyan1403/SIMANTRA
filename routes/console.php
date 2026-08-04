2f2e64d8eb1079ee2310015ab0c405f67e93cce5
supyan1403 <supyansauri47@gmail.com>
2026-08-04 13:35:42 +0700

    register artisan commands mantra:import and import:mantra in console routes

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,11 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('mantra:import {path?}', function ($path = null) {
+    $this->call(\App\Console\Commands\ImportMantraCommand::class, array_filter(['path' => $path]));
+})->purpose('Import data MANTRA Excel ke database');
+
+Artisan::command('import:mantra {path?}', function ($path = null) {
+    $this->call(\App\Console\Commands\ImportMantraCommand::class, array_filter(['path' => $path]));
+})->purpose('Import data MANTRA Excel ke database');
